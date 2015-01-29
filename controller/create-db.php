09f6fdd9431d473d49ec0c60ccb14c343d373b07
jazmine ochoa <jazzy.ochoa@yahoo.com>
2015-01-29 11:25:07 -0800

    FINISHED 1 of the milestone 3

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,13 +1,16 @@
 <?php
-require_once (__DIR__ . "../model/database.php");
+require_once (__DIR__ . "/../model/database.php");
 
 $connection = new mysqli ($host, $username ,$password);
 
 if($connection-> connect_error) {
     die("Error: . $connection->connect_error");
 }
-else{
-    echo "sucess" . $connection->host_info;
+
+$exists = $connection->select_db($database);
+
+if(!$exists){
+    echo"Database does not exists";
 }
 
 $connection->close();
