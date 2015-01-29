0bd268aa21f5bad5c92aea10aa0324ab78f2b1a5
jazmine ochoa <jazzy.ochoa@yahoo.com>
2015-01-29 11:10:34 -0800

    FINISHED MILESTONES 1&2

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,3 +1,13 @@
 <?php
-require_once ("../model/database.php");
+require_once (__DIR__ . "../model/database.php");
 
+$connection = new mysqli ($host, $username ,$password);
+
+if($connection-> connect_error) {
+    die("Error: . $connection->connect_error");
+}
+else{
+    echo "sucess" . $connection->host_info;
+}
+
+$connection->close();
