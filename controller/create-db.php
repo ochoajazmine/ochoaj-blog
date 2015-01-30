7a41a0d6d9507b55160e76d163eb2aab1545609b
jazmine ochoa <jazzy.ochoa@yahoo.com>
2015-01-30 11:26:34 -0800

    on 15

--- controller/create-db.php
+++ controller/create-db.php
@@ -10,7 +10,28 @@ if($connection-> connect_error) {
 $exists = $connection->select_db($database);
 
 if(!$exists){
-    echo"Database does not exists";
+    $query = $connection->query("CREATE DATABASE $database");
+    
+    if($query){
+        echo "sucessfully created database:" . $database;
+    }
+} 
+else{
+    echo "Database already exist  ";
+}
+
+$query = $connection->query("CREATE TABLE posts("
+        ." id int(11) NOT NULL AUTO_INCREMENT,"
+        ."title varchar(255) NOT NULL,"
+        ."post text NOT NULL,"
+        ."PRIMARY KEY (ID))");
+
+if($query){
+    echo "succesfully created table: posts";
+}
+
+else{
+    echo "$connection->error";
 }
 
 $connection->close();
