4dbc79412363b1ddb57c9407d618097de2bc17da
jazmine ochoa <jazzy.ochoa@yahoo.com>
2015-02-04 11:05:46 -0800

    done

---
+++ view/form.php
@@ -0,0 +1,17 @@
+<h1>create blog post</h1>
+
+<form>
+    <div>
+        <label for="title"> Title:</label>
+        <input type="=text" name="title"/>
+    </div>
+    
+    <div>
+        <label for="post">post:</label>
+        <textarea name="post"></textarea>
+    </div>
+    
+    <div>
+        <button type="submit">submit</button> 
+    </div>
+</form>
