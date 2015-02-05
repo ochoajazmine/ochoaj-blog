8fb5a07804d8700c96723d940f5b578d01216791
jazmine ochoa <jazzy.ochoa@yahoo.com>
2015-02-05 11:19:41 -0800

    21

---
+++ controller/create-posts.php
@@ -0,0 +1,8 @@
+<?php
+$title = filer_input(INPUT_POST, "title,",FILTER_SANITIZE_STRING) ;
+$post =filter_input(INPUT_POST,"post", FILTER_SANITIZE_STRING);
+
+echo "<p>title: $title</p>";
+echo "<p>post: $post</p>";
+
+
