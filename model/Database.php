ac3c65e0d7af7d0ac8f4d74f0b417c6b9df2f81b
jazmine ochoa <jazzy.ochoa@yahoo.com>
2015-02-11 11:26:09 -0800

    no message

---
+++ model/Database.php
@@ -0,0 +1,29 @@
+<?php
+class Database{
+    private $connection;
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+    
+    public function __construt($host, $username, $passsword, $database) {
+       $this->host = $host;
+       $this->username = $username;
+       $this->password = $password;
+       $this->database = $database;
+        
+    }
+    
+    public function openconnection(){
+        $this->
+        
+    }
+    
+    public function closeconnection(){
+        
+    }
+    
+    public function query($string){
+        
+    }
+}
